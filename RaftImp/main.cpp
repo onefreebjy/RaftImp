9622e7723fd92cd91abd567dfe495b047d174d23
Bai Jinyan <onefreebjy@gmail.com>
2016-05-06 21:26:24 +0300

    Initial Commit

---
+++ RaftImp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RaftImp
+//
+//  Created by BaiJinyan on 6/5/2016.
+//  Copyright © 2016 BaiJinyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
