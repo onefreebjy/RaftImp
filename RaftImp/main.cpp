4a5dc3d103e3b1c353347e354601bdfed4e4b928
Bai Jinyan <onefreebjy@gmail.com>
2016-05-07 16:15:40 +0300

    Test Client&Server model

--- RaftImp/main.cpp
+++ RaftImp/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+#include <unistd.h>
+
+#include <msgpack.hpp>
+#include <zmq.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    //  Prepare our context and socket
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
+    
+    while (true) {
+        zmq::message_t request;
+        
+        //  Wait for next request from client
+        socket.recv (&request);
+        std::cout << "Received Hello" << std::endl;
+        
+        //  Do some 'work'
+        sleep(1);
+        
+        //  Send reply back to client
+        zmq::message_t reply (5);
+        memcpy (reply.data (), "World", 5);
+        socket.send (reply);
+    }
     return 0;
 }
