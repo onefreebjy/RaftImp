00eeb81da74c0674881b02c6f9fd6c0354924930
Bai Jinyan <onefreebjy@gmail.com>
2016-05-08 19:47:36 +0300

    test zmq::poll

--- RaftImp/main.cpp
+++ RaftImp/main.cpp
@@ -12,26 +12,63 @@
 #include <msgpack.hpp>
 #include <zmq.hpp>
 
+using namespace std;
+
+#define MAX_RUNTIME 20
+
+static void watchdog(int signro){
+    cout<<"alarm"<<endl;
+    exit(signro);
+}
+
 int main(int argc, const char * argv[]) {
     //  Prepare our context and socket
     zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("tcp://*:5555");
     
-    while (true) {
-        zmq::message_t request;
+    //  Bind to a port to wait for request from client
+    zmq::socket_t receiver (context, ZMQ_REP);
+    receiver.bind ("tcp://127.0.0.1:5555");
+    
+    //  Connect to publisher
+    zmq::socket_t subscriber(context, ZMQ_SUB);
+    subscriber.connect("tcp://localhost:5556");
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "10001", 5);
+    
+    //  Initialize poll set
+    zmq::pollitem_t items[] = {
+        {(void *)receiver, 0, ZMQ_POLLIN, 0},
+        {(void *)subscriber, 0, ZMQ_POLLIN, 0}
+    };
+    
+    //  Set up timer
+    if (signal(SIGALRM, watchdog) == SIG_ERR)
+        exit(2);
+    alarm(MAX_RUNTIME);
+    
+    while (1){
+        zmq::message_t message;
+        zmq::poll(items, 2, -1);
         
-        //  Wait for next request from client
-        socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
+        if (items[0].revents & ZMQ_POLLIN){
+            //  Wait for next request from client
+            receiver.recv (&message);
+            char buf[15];
+            memcpy(buf, message.data(), message.size());
+            cout << "Received " << buf << endl;
+    
+            //  Send reply back to client
+            strcpy(buf, "World");
+            zmq::message_t reply (strlen(buf));
+            memcpy (reply.data (), buf, strlen(buf));
+            receiver.send (reply);
+        }
         
-        //  Do some 'work'
-        sleep(1);
-        
-        //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy (reply.data (), "World", 5);
-        socket.send (reply);
+        if (items[1].revents & ZMQ_POLLIN){
+            subscriber.recv(&message);
+            char buf[15];
+            memcpy(buf, message.data(), message.size());
+            cout << "Received " << buf << endl;
+        }
     }
     return 0;
 }
